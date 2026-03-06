1dc1c461b0b62c2ee03f74737484a73bc53a7e36
Guilherme Samuel <guisamuel800@gmail.com>
2026-03-06 14:38:36 -0300

    Add yaw rotation

--- canbot.ino
+++ canbot.ino
@@ -1,21 +1,33 @@
-#include<Servo.h>
+#include <Servo.h>
+
+#define JOYSTICK_X_PIN A0
+#define JOYSTICK_Y_PIN A1
+#define ANALOG_BUTTON_PIN A4
+#define ARM_PIN 10
+#define YAW_PIN 9
+#define CLAW_PIN 8
+
+Servo yaw;
+Servo arm;
 Servo claw;
 
+int xValue, yValue, angleX, angleY;
+
 void setup() {
-	claw.attach(6);
-}
+  yaw.attach(YAW_PIN);
+  arm.attach(ARM_PIN);
+	claw.attach(CLAW_PIN);
 
-void OpenClaw() {
-	claw.write(90);
-}
-
-void CloseClaw() {
-	claw.write(120);
+  pinMode(JOYSTICK_X_PIN, INPUT);
+  pinMode(JOYSTICK_Y_PIN, INPUT);
 }
 
 void loop() {
-  OpenClaw();
-  delay(1000);
-  CloseClaw();
-  delay(1000);
+  xValue = analogRead(JOYSTICK_X_PIN);
+  yValue = analogRead(JOYSTICK_Y_PIN);
+  
+  angleX = map(xValue, 0, 1023, 0, 180);
+  angleY = map(yValue, 0, 1023, 0, 180);
+
+  yaw.write(angleX);
 }
