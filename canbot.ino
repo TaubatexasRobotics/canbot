b40021e6561403da63a6217e048def2ae149b6a5
Guilherme Samuel <guisamuel800@gmail.com>
2026-03-06 23:23:36 -0300

    Add claw open with push button

--- canbot.ino
+++ canbot.ino
@@ -2,14 +2,16 @@
 
 #define JOYSTICK_X_PIN A0
 #define JOYSTICK_Y_PIN A1
-#define ANALOG_BUTTON_PIN A4
-#define ARM_PIN 10
+#define JOYSTICK_PUSH_PIN 7
+#define ARM_PIN 8
 #define YAW_PIN 9
-#define CLAW_PIN 8
+#define CLAW_PIN 10
 
 Servo yaw;
 Servo arm;
 Servo claw;
+
+bool clawOpened;
 
 int xValue, yValue, angleX, angleY;
 
@@ -17,9 +19,11 @@ void setup() {
   yaw.attach(YAW_PIN);
   arm.attach(ARM_PIN);
 	claw.attach(CLAW_PIN);
+  clawOpened = false;
 
   pinMode(JOYSTICK_X_PIN, INPUT);
   pinMode(JOYSTICK_Y_PIN, INPUT);
+  pinMode(JOYSTICK_PUSH_PIN, INPUT_PULLUP);
 }
 
 void loop() {
@@ -30,4 +34,14 @@ void loop() {
   angleY = map(yValue, 0, 1023, 0, 180);
 
   yaw.write(angleX);
+  arm.write(angleY);
+
+  if(digitalRead(JOYSTICK_PUSH_PIN) == LOW) {
+    clawOpened = !clawOpened;
+
+    if(clawOpened) claw.write(0);
+    else claw.write(120);
+  }
+
+  delay(20);
 }
